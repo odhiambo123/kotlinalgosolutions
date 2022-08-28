279bf37b9b202c8ad641a979121f3fd1d47eef56
David Odhiambo <odhiambodave@live.com>
2022-08-28 10:28:16 -0700

    change project type from android

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -1,5 +1,7 @@
 package com.davidodhiambo.kotlinalgosolutions
 
+
+
 class Functions {
     //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
     //You may assume that each input would have exactly one solution, and you may not use the same element twice.
